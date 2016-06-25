484e32098abb6ca8e897beae86d12741554dc5b8
michael-r-may <michael_r_may@myfastmail.com>
2016-06-25 17:18:25 +0100

    Putting some empty Swift files in place so the app compile and runs the tests without issue. We need this so we have a good known state before we start the talk, but no actual content to start with

---
+++ AlergyTracker/Supporting Files/TokenSwiftFile.swift
@@ -0,0 +1,11 @@
+//
+//  TokenSwiftFile.swift
+//  AllergyTracker
+//
+//  Created by Developer on 2016/06/25.
+//  Copyright © 2016 Radical Robot. All rights reserved.
+//
+
+import Foundation
+
+// Needed to make the import AllergyTracker call in the tests work (whilst we have no other swift files in the app)
---
+++ AlergyTrackerTests/DummyTests.swift
@@ -0,0 +1,13 @@
+//
+//  DummyTests.swift
+//  AllergyTracker
+//
+//  Created by Developer on 2016/06/25.
+//  Copyright © 2016 Radical Robot. All rights reserved.
+//
+
+import XCTest
+
+class DummyTests: XCTestCase {
+
+}
