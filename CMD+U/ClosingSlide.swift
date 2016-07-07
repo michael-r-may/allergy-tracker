c03fa82f7d479b4fa6509b44cfcab2e60c46c5db
michael-r-may <michael_r_may@myfastmail.com>
2016-07-07 22:37:21 +0100

    Readability tweaks

--- CMD+U/ClosingSlide.swift
+++ CMD+U/ClosingSlide.swift
@@ -1,17 +1,20 @@
 import Foundation
 
 struct ThankYou {
-    let talk = TalkSummary(title:  "Testing An Untested App",     format: "Live Coding 😀")
+    let talk = TalkSummary(title:  "Testing An Untested App",
+                           format: "Live Coding 😀")
     
-    let speaker = Speaker(name: "Michael May",  twitter:   "@codermay")
+    let speaker = Speaker(name: "Michael May",
+                          twitter: "@codermay")
     
-    let repoURL = NSURL(string: "https://github.com/radicalrobot/allergy-tracker")!
-    let appSource = GitHubRepo(sourceURL: repoURL, author: "Emily Toop")
+    let appSource = GitHubRepo(
+        sourceURL:"https://github.com/radicalrobot/allergy-tracker",
+        author: "Emily Toop")
     
     let apologiesToEU = (quote: "Ask most Londoners what we love about this city and we'll say one thing; it's diversity...Our city has not changed. It remains a place that cherishes human beings of all nationalities and ethnic backgrounds.", author: "Time Out Magazine")
 }
 
 struct GitHubRepo {
-    let sourceURL: NSURL
+    let sourceURL: String
     let author: String
 }
