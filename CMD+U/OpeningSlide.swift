ba85192944ded3b967abd22883793884b82b834b
michael-r-may <michael_r_may@myfastmail.com>
2016-07-08 06:38:22 +0100

    More Readability tweaks

--- CMD+U/OpeningSlide.swift
+++ CMD+U/OpeningSlide.swift
@@ -2,14 +2,17 @@ import Foundation
 import CoreLocation
 
 struct Event {
-    let talk = TalkSummary(title: "Testing An Untested App",      format: "Live Coding 😰")
+    let talk = TalkSummary(title: "Testing An Untested App",
+                           format: "Live Coding 😰")
     
-    let speaker = Speaker(name: "Michael May",     twitter: "@codermay")
+    let speaker = Speaker(name: "Michael May",
+                          twitter: "@codermay")
     
     let conference = Conference(name: "CMD+U",
                                 location: CLLocation(latitude: 40.33241, longitude: -0.194569),
                                 date: NSDate(timeIntervalSince1970: 1467972000000))
 }
+
 
 struct TalkSummary {
     let title: String
